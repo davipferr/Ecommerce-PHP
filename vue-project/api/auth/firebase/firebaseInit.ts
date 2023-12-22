3332ac6b38b638b4ac2ff65f545090aefcdab966
Davi Pinto Ferreira <davipferr@gmail.com>
2023-12-22 14:45:16 -0300

    coloca a configuração do firebase no .env

--- vue-project/api/auth/firebase/firebaseInit.ts
+++ vue-project/api/auth/firebase/firebaseInit.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGhWq3EqG909lXOacUE2BhNzsJvsEYwOw",
-  authDomain: "vue-app-6e7de.firebaseapp.com",
-  projectId: "vue-app-6e7de",
-  storageBucket: "vue-app-6e7de.appspot.com",
-  messagingSenderId: "626210091980",
-  appId: "1:626210091980:web:09c11ea9a68cffa00d6369",
-  measurementId: "G-D2KL9BQV8B"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MESSAGING_MEASUREMENT_ID,
 };
 
 export const initializeFireBase = () => {
